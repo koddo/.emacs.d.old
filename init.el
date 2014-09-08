2966e308551e0eeaf3e808325604e1daaf8db044
Alex Scherbanov <alex@Alexs-MacBook-Pro.local>
2014-09-08 05:12:44 +0400

    adding some files as a first step

---
+++ init.el
@@ -0,0 +1,7 @@
+; (load "~/.emacs.d/init_packages.el")
+; (load "~/.emacs.d/init_el-get.el")
+
+
+(add-to-list 'load-path "~/.emacs.d/ym-packages/org-mode/contrib/lisp")
+(add-to-list 'load-path "~/.emacs.d/ym-packages/org-mode/lisp")
+
---
+++ unused_init_el-get.el
@@ -0,0 +1,21 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+(setq ym-el-get-packages
+      '(   ;;diminish
+        autopair
+	ledger-mode
+        ))
+
+(setq el-get-allow-insecure nil)
+
+(el-get 'sync ym-el-get-packages)
