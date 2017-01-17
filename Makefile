897bd20da587eaa8ea19c741b3432dbcaef07665
Alex Scherbanov <alex@egotv.ru>
2017-01-17 20:11:51 +0300

    Squashed 'ym-packages/macrostep/' changes from 51d5b6af7..424e3734a
    424e3734a Merge pull request #16 from fice-t/master
    2d216bac2 Merge pull request #19 from fice-t/autoload
    7b2c1f375 Load all autoloaded macros
    29fd92496 Move overlay face to a different overlay
    e53761269 compmiler-macro changed to compiler-macro
    1e2593279 add lib/.nosearch
    a3338d351 Make test script exit non-zero on failure
    aa59895b4 Add .travis.yml
    b65cc9616 Makefile tweaks
    d48e52bd1 Add tests for compiler-macro expansion
    9a6b04a5f Restore Emacs 23 compatibility
    d1e954de8 Attempt to shell out to "indent" in macrostep-c
    6c8ae5bc6 Remove call to `define-error` for Emacs <24.4
    d6aa67c60 Be consistent about only using prefixed `cl-lib`
    03d4d7bb4 Add basic C macro expansion via cmacexp.el
    bf87b7c1e Whitespace cleanup
    2473a6f76 Pass START and END args to sexp-at-point-function
    94d40f223 Check that compiler-macros return a changed form
    c594c0939 `cl-macs' is no longer needed at run-time
    704f1645d Merge branch 'generic'
    2c5bda25f Update README, bump version number
    a8e730e9b Improve Makefile
    4b382cd9e Identify Elisp compiler-macros more selectively
    47cbacb7f Define `macrostep-propertize` before using it
    84e5bfaec Make tests pass again
    31a23ebd0 Toggle separate-buffer expansion with prefix (#8)
    8f74a4be2 Toggle separate-buffer expansion with prefix (#8)
    80ae10125 Fix SLIME functions for updated generic interface
    2bdc946e9 Remove dynamic binding of `macrostep-environment'.
    bbfb03372 Reorganise and document generic interface
    1edee8518 Reorganise and comment
    b1c1230b7 More tests
    e582ca7a5 Fix trivial bug printing a single non-list form
    dd14d5c0e Remove unused function `bindings-to-environment`
    aa40d97ca Test expansion within Elisp macro-defining macros
    2390cec66 slime-sexp-at-point may return nil, deal with it
    817322b32 Properly place the macrostep[-compiler]-macro-face
    a0ae61c51 Properly handle forms not present in the expansion
    afed3cfda MARKER-CHAR-POSITION => MARKER-CHAR-ID
    59972eb8e Fix handling of subforms contained within other subforms
    d7991b4a9 Track forms using the printer rather than the reader
    0b1c0fe4c Add swank/gray:stream-read-char-no-hang implementation
    5e0c28ffa Establish *macroexpand-printer-bindings* before pretty printing
    aa4736c06 Fix some silly errors
    059dda635 Also hook into slime-repl-mode
    ff3888f9a Use a form-tracking stream rather than gensym substitution
    39e809a58 swank-macrostep: call PROVIDE at the end
    d9c762981 Improvements to Elisp environment handling
    c61b83654 Use SB-WALKER:WALK-FORM to collect macro forms
    1be6c4d68 Extract Elisp macro environments accurately
    31e1dc27b Detect Elisp macro forms by advising `macroexpand`
    b449cd012 Restore compiler-macro expansion under SLIME
    874c79073 WIP: smarter SLIME macroexpansion
    c74899638 Tweak macrostep-slime-macro-form-p
    56802789e Gross hack to macrostep--slime-propertize-macros
    f34deb73f Specify text bounds when propertizing SLIME macros
    a478a3a3e Bind inhibit-read-only instead of buffer-read-only
    c93c2d6c8 Language-agnostic macro-form boundaries
    9a534df7a Make macrostep-slime-insert more robust
    0c8b64c0f Position macrostep-slime-macro-form-p in the correct package
    522226f7e Improve `macrostep-slime-macro-form-p`
    e9a95f47a Fix `macrostep--macro-form-info`
    3f965b0ee Use new `macrostep-macro-start` text property
    e35b61d74 Add compiler-macro support for SLIME
    3062d4c35 Add support for compiler macros
    4ea178aa5 Simplify overlay collapsing
    8d72b622e Fix indentation in new macrostep-pp function
    3766ffbb8 Hack to propertize macros in SLIME expansions
    352b6d214 Minor improvements to SLIME prototype
    96e0c02ee WIP: Add generic interface for other languages
    9d2c58c05 Add option to expand in a separate buffer
    1c0114505 Merge pull request #9 from xuchunyang/master
    251969224 Restore narrowing
    8950313a4 Bump version number and changelog for 0.8
    
    git-subtree-dir: ym-packages/macrostep
    git-subtree-split: 424e3734a1ee526a1bd7b5c3cd1d3ef19d184267

--- Makefile
+++ Makefile
@@ -1,4 +1,17 @@
-test:
-	emacs --batch --load macrostep-test.el
+EMACS ?= emacs
 
-.PHONY: test
+all: macrostep.elc macrostep-c.elc
+
+clean:
+	rm -f *.elc
+
+test: clean all
+	$(EMACS) --batch -L . --load macrostep-test.el
+
+sandbox: all
+	$(EMACS) -Q -L . --load macrostep --load macrostep-c
+
+%.elc: %.el
+	$(EMACS) --batch -L . --funcall batch-byte-compile "$<"
+
+.PHONY: test all clean
--- macrostep-test.el
+++ macrostep-test.el
@@ -1,6 +1,6 @@
 ;;; macrostep-test.el --- tests for macrostep.el
 
-;; Copyright (C) 2014 Jon Oddie <j.j.oddie@gmail.com>
+;; Copyright (C) 2014-2015 Jon Oddie <j.j.oddie@gmail.com>
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -18,8 +18,19 @@
 ;; along with this program.  If not, see `http://www.gnu.org/licenses/'.
 
 
+(eval-and-compile
+  (require 'ert nil t)
+  (require 'ert "lib/ert"))
+(require 'rx)
+(require 'macrostep)
+(require 'macrostep-c)
+
+
+;;;; Conveniences for defining tests
+
 (defmacro macrostep-with-text (object &rest forms)
-  (declare (indent 1))
+  (declare (indent 1)
+           (debug (&rest form)))
   `(with-temp-buffer
      (emacs-lisp-mode)
      (let ((print-level nil)
@@ -29,7 +40,13 @@
          (print ,object))
        ,@forms)))
 
-(defmacro macrostep-should-expand (form expansion)
+(defun macrostep-goto (text &optional from-point-min)
+  (when from-point-min (goto-char (point-min)))
+  (let ((search-spaces-regexp "[[:space:]\n]+"))
+    (re-search-forward (regexp-quote text))
+    (goto-char (match-beginning 0))))
+
+(defmacro macrostep-should-expand (form expansion &optional leave-expanded-p)
   `(save-excursion
      (goto-char (point-min))
      (let ((print-level nil)
@@ -42,8 +59,11 @@
               (should
                (equal (read (current-buffer))
                       ,expansion)))
-         (macrostep-collapse-all)))))
+	 (unless ,leave-expanded-p
+	   (macrostep-collapse-all))))))
 
+
+;;;; Tests
 (ert-deftest macrostep-expand-defmacro ()
   (defmacro macrostep-dummy-macro (&rest args)
     `(expansion of ,@args))
@@ -57,9 +77,37 @@
     '(macrostep-dummy-macro (first (argument)) second (third argument))
     '(expansion of (first (argument)) second (third argument)))))
 
+(ert-deftest macrostep-expand-and-collapse ()
+  (dolist (expander
+            (list
+             (lambda (sexp _env) sexp)
+             (lambda (sexp _env) `(progn ,sexp ,sexp))
+             (lambda (sexp _env) `(long
+                              (complicated
+                               (expansion of ,sexp () ())
+                               (with trailing forms))))))
+    (let ((macrostep-expand-1-function expander)
+          (macrostep-macro-form-p-function
+           (lambda (&rest _) t)))
+      (macrostep-with-text
+          '(progn
+            (first form)
+            (second form)
+            (third (nested form)))
+        (macrostep-goto "(first ")
+        (let ((original-text (buffer-string)))
+          (dotimes (i 10)
+            (dotimes (j i)
+              (macrostep-expand))
+            (dotimes (j i)
+              (macrostep-collapse))
+            (should (null macrostep-overlays))
+            (should (string= (buffer-string)
+                             original-text))))))))
+
 (ert-deftest macrostep-expand-macrolet ()
   (macrostep-with-text
-      '(macrolet
+      '(cl-macrolet
         ((test (&rest args) `(expansion of ,@args)))
         (first body form)
         (second body form)
@@ -72,7 +120,7 @@
 (ert-deftest macrostep-expand-macrolet-2 ()
   (macrostep-with-text
       ;; Taken from org-notify.el.
-      '(macrolet ((get (k) `(plist-get list ,k))
+      '(cl-macrolet ((get (k) `(plist-get list ,k))
                   (pr (k v) `(setq result (plist-put result ,k ,v))))
         (let* ((list (nth 1 heading))      (notify (or (get :notify) "default"))
                (deadline (org-notify-convert-deadline (get :deadline)))
@@ -126,7 +174,7 @@
 
 (ert-deftest macrostep-expand-shadowed-macrolet ()
   (macrostep-with-text
-      '(macrolet
+      '(cl-macrolet
         ((test-macro (&rest forms) (cons 'shadowed forms))
          (test-macro (&rest forms) (cons 'outer-definition forms)))
         (test-macro first (call))
@@ -140,10 +188,10 @@
      '(test-macro (second (call)))
      '(inner-definition (second (call))))))
 
-(ert-deftest macrostep-environnment-at-point ()
+(ert-deftest macrostep-environment-at-point ()
   (macrostep-with-text
       ;; Taken from org-notify.el.
-      '(macrolet ((get (k) `(plist-get list ,k))
+      '(cl-macrolet ((get (k) `(plist-get list ,k))
                   (pr (k v) `(setq result (plist-put result ,k ,v))))
         (body forms))
     (search-forward "(body")
@@ -161,6 +209,30 @@
         (apply (cdr (assq 'get env)) '(:begin))
         '(plist-get list :begin))))))
 
+(ert-deftest macrostep-environment-at-point-2 ()
+  (defmacro macrostep-with-dummy (&rest body)
+    `(cl-macrolet ((dummy (&rest forms) `(expansion of ,@forms)))
+       ,@body))
+  (macrostep-with-text
+      '(macrostep-with-dummy
+        (body)
+        (forms)
+        (cl-loop for i from 6 to 10
+         do (something)))
+    (macrostep-goto "(macrostep-with-dummy" t)
+    (should (null (macrostep-environment-at-point)))
+
+    (dolist (place (list "(body)" "dy)" "(forms)" "rms)"
+                         "(something)"))
+      (macrostep-goto place)
+      (let* ((env (macrostep-environment-at-point))
+             (dummy-defn (cdr (assq 'dummy env))))
+        (should dummy-defn)
+        (should (functionp dummy-defn))
+        (should (equal
+                 (funcall dummy-defn 'lorem 'ipsum)
+                 `(expansion of lorem ipsum)))))))
+
 (ert-deftest macrostep-print-sexp ()
   (cl-macrolet ((should-print (form string)
                   `(should (equal
@@ -168,6 +240,7 @@
                               (macrostep-print-sexp ,form)
                               (buffer-string))
                             ,string))))
+    (should-print nil "nil")
     (should-print 'symbol "symbol")
     (should-print '(single-element-list) "(single-element-list)")
     (should-print '(two-element list) "(two-element list)")
@@ -182,43 +255,208 @@
     (should-print '`(backquoted (form) ,with ,@splices)
                   "`(backquoted (form) ,with ,@splices)")))
 
-(ert-deftest macrostep-print-sexp-macrolet-environment ()
+(ert-deftest macrostep-pp-macrolet-environment ()
   (with-temp-buffer
     (emacs-lisp-mode)
-    (save-excursion
-      (macrostep-print-sexp
-       '(macrolet ((some-macro (&rest forms) (cons 'progn forms)))
-         (some-macro with (arguments))
-         (intervening body forms)
-         (some-macro with (more) (arguments))))
-      (cl-flet ((search (text)
-                  (goto-char (point-min))
-                  (search-forward text)
-                  (goto-char (match-beginning 0))
-                  ;; Leave point on the head of the form
-                  (forward-char)))
-        ;; The occurrence of "(some-macro" in the binding list should
-        ;; not be fontified as a macro form
-        (search "(some-macro (&rest")
-        (should-not
-         (eq (get-char-property (point) 'font-lock-face)
-             'macrostep-macro-face))
+    (macrostep-pp
+     '(cl-macrolet ((some-macro (&rest forms) (cons 'progn forms)))
+	(some-macro with (arguments))
+	(intervening body forms)
+	(some-macro with (more) (arguments)))
+     nil)
+    (cl-labels ((search (text)
+                (macrostep-goto text t)
+                ;; Leave point on the head of the form
+                (forward-char)))
+      ;; The occurrence of "(some-macro" in the binding list should
+      ;; not be fontified as a macro form
+      (search "(some-macro (&rest")
+      (should-not
+       (eq (get-char-property (point) 'font-lock-face)
+           'macrostep-macro-face))
 
-        ;; However, the two occurrences in the body of the macrolet should be.
-        (search "(some-macro with (arguments)")
-        (should
-         (eq (get-char-property (point) 'font-lock-face)
-             'macrostep-macro-face))
+      ;; However, the two occurrences in the body of the macrolet should be.
+      (search "(some-macro with (arguments)")
+      (should
+       (eq (get-char-property (point) 'font-lock-face)
+           'macrostep-macro-face))
 
-        (search "(some-macro with (more)")
-        (should
-         (eq (get-char-property (point) 'font-lock-face)
-             'macrostep-macro-face))))))
+      (search "(some-macro with (more)")
+      (should
+       (eq (get-char-property (point) 'font-lock-face)
+           'macrostep-macro-face)))))
 
+(ert-deftest macrostep-expand-macro-defined-macros ()
+  (defmacro with-local-dummy-macro (&rest body)
+    `(cl-macrolet ((dummy (&rest args) `(expansion (of) ,@args)))
+       ,@body))
+  (macrostep-with-text
+      '(with-local-dummy-macro
+        (dummy form (one))
+        (dummy (form two)))
+    (macrostep-should-expand
+     '(dummy form (one))
+     '(expansion (of) form (one)))
+    (macrostep-should-expand
+     '(dummy (form two))
+     '(expansion (of) (form two)))))
+
+(ert-deftest macrostep-expand-in-separate-buffer ()
+  (defmacro macrostep-dummy-macro (&rest args)
+    `(expansion of ,@args))
+  (let ((macrostep-expand-in-separate-buffer t))
+    (macrostep-with-text
+        '(progn
+          (first form)
+          (second form)
+          (macrostep-dummy-macro (some (arguments)))
+          (final form))
+      (let ((original-buffer (current-buffer)))
+        (search-forward "(macrostep-dummy-macro")
+        (macrostep-expand)
+        (should (not (equal (current-buffer) original-buffer)))
+        (should macrostep-expansion-buffer)
+        (should (equal (read (copy-marker (point)))
+                       '(expansion of (some (arguments)))))))))
+
+(ert-deftest macrostep-expand-macrolet-in-separate-buffer ()
+  (let ((macrostep-expand-in-separate-buffer t))
+    (macrostep-with-text
+        '(cl-macrolet
+          ((dummy-macro-1 (&rest args)
+            `(dummy-macro-2 ,@args))
+           (dummy-macro-2 (&rest args)
+            `(expansion of ,@args)))
+          (dummy-macro-1 (some (arguments))))
+      (let ((original-buffer (current-buffer)))
+        (macrostep-goto "(dummy-macro-1 (some")
+
+        (macrostep-expand)
+        (should (not (equal (current-buffer) original-buffer)))
+        (should macrostep-expansion-buffer)
+        (should (equal (read (copy-marker (point)))
+                       '(dummy-macro-2 (some (arguments)))))
+
+        (macrostep-expand)
+        (should (equal (read (copy-marker (point)))
+                       '(expansion of (some (arguments)))))
+
+        (macrostep-collapse)
+        (should (equal (read (copy-marker (point)))
+                       '(dummy-macro-2 (some (arguments)))))))))
+
+(ert-deftest macrostep-expand-compiler-macros ()
+  "Test that compiler-macros are expanded"
+  ;; definitions
+  (defun macrostep-dummy-function (&rest args)
+    args)
+  (cl-define-compiler-macro macrostep-dummy-function (&rest args)
+    `(compile-time expansion of ,@args))
+
+  (macrostep-with-text
+      '(progn
+        (first body form)
+        (macrostep-dummy-function first second third)
+        (remaining body forms))
+    (macrostep-should-expand
+     '(macrostep-dummy-function first second third)
+     '(compile-time expansion of first second third))))
+
+(ert-deftest macrostep-fontify-compiler-macros ()
+  "Test that compiler-macros are fontified in macro-expansions"
+  ;; definitions
+  (defmacro macrostep-dummy-macro (&rest args)
+    `(expansion including (macrostep-dummy-function ,@args)))
+  (defun macrostep-dummy-function (&rest args)
+    args)
+  (cl-define-compiler-macro macrostep-dummy-4 (&rest args)
+    `(compile-time expansion of ,@args))
+
+  (macrostep-with-text
+      '(progn
+        (first body form)
+        (second body form)
+        (macrostep-dummy-macro first second third)
+        (remaining body forms))
+    (macrostep-should-expand
+     '(macrostep-dummy-macro first second third)
+     '(expansion including (macrostep-dummy-function first second third))
+     t)
+    (macrostep-goto "(macrostep-dummy-function")
+    (should
+     (eq (get-char-property (1+ (point)) 'font-lock-face)
+	 'macrostep-compiler-macro-face))
+    
+    (macrostep-should-expand
+     '(macrostep-dummy-function first second third)
+     '(compile-time expansion of first second third))))
+
+
+;;;; Tests for C macro expansion
+
+(defun macrostep-lax-looking-at (string)
+  (let* ((string-sans-whitespace
+          (replace-regexp-in-string (rx (one-or-more whitespace)) "" string))
+         (regexp
+          (cl-loop
+           for char across string-sans-whitespace
+           concat (rx-to-string char t)
+           concat "[[:space:]]*")))
+    (looking-at regexp)))
+
+(defmacro macrostep-lax-should-expand (string)
+  `(progn
+     (macrostep-expand)
+     (should (macrostep-lax-looking-at ,string))
+     (macrostep-collapse)))
+
+(ert-deftest macrostep-expand-c-macros ()
+  (with-temp-buffer
+    (insert
+     ;; A random example adapted from Emacs's src/lisp.h.
+     "
+#define eassert(cond) ((void) (false && (cond))) /* Check COND compiles.  */
+#define lisp_h_XLI(o) (o)
+#define lisp_h_XUNTAG(a, type) ((void *) (intptr_t) (XLI (a) - (type)))
+#define XLI(o) lisp_h_XLI (o)
+#define XUNTAG(a, type) lisp_h_XUNTAG (a, type)
+
+INLINE struct Lisp_String *
+XSTRING (Lisp_Object a)
+{
+  eassert (STRINGP (a));
+  return XUNTAG (a, Lisp_String);
+}")
+    (c-mode)
+
+     ;; Test macro-expansion with point at the beginning of the macro
+    (macrostep-goto "eassert (STRINGP (a))" t)
+    (macrostep-lax-should-expand
+     "((void) (false && (STRINGP (a))))")
+
+    ;; Test with point inside a nested macro call: result should be
+    ;; the same, since point will move up before the outermost macro
+    (macrostep-goto "STRINGP")
+    (macrostep-lax-should-expand
+     "((void) (false && (STRINGP (a))))")
+
+    ;; Test with point in the middle of a symbol
+    (macrostep-goto "XUNTAG (a, Lisp_String)" t)
+    (forward-char 3)
+    (macrostep-lax-should-expand
+     "((void *) (intptr_t) ((a) - (  Lisp_String)))")
+
+    ;; Test with point at symbol-end
+    (macrostep-goto "XUNTAG (a, Lisp_String)" t)
+    (forward-sexp)
+    (macrostep-lax-should-expand
+     "((void *) (intptr_t) ((a) - (  Lisp_String)))")))
+
+
 
 (when noninteractive
   (load-file (expand-file-name "macrostep.el"
                                (file-name-directory load-file-name)))
-  (ert-run-tests-batch "^macrostep"))
-
-
+  (let ((stats (ert-run-tests-batch "^macrostep")))
+        (if noninteractive
+            (kill-emacs (ert-stats-completed-unexpected stats)))))
