64997f856c404467e1c872514cd5c7157493e5c5
Alex Scherbanov <alex@egotv.ru>
2020-05-11 16:47:10 +0300

    remove yasnippet folder

--- yasnippet/emacs-lisp-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `emacs-lisp-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'emacs-lisp-mode
-                     '(("f" "(defun ${1:name} (${2:args})\n       ${3:(interactive)\n       }$0\n       )" "defun" nil nil nil "/Users/alex/.emacs.d/yasnippet/emacs-lisp-mode/defun" nil nil)))
-
-
-;;; Do not edit! File generated at Sun May 14 16:52:11 2017
--- yasnippet/markdown-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `markdown-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'markdown-mode
-                     '(("ymjekyllposttemplate" "---\nlayout: post\ntitle:  \"$0\"\ndate:   `(format-time-string \"%Y-%m-%d %H:%M:%S %z\")`\ntags:   []\ncomments: true\ndisqus_identifier: `(first (process-lines \"uuidgen\"))`\n\n---\n\n" "ymjekyllposttemplate" nil nil nil "/Users/alex/.emacs.d/yasnippet/markdown-mode/ymjekyllposttemplate" nil nil)))
-
-
-;;; Do not edit! File generated at Sun May 14 16:52:11 2017
