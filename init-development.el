dc753720cdece9d4f9365ec0d575b3a98c0cf898
koddo <2563454+koddo@users.noreply.github.com>
2024-06-24 23:20:21 +0300

    wip

--- init-development.el
+++ init-development.el
@@ -1,9 +1,4 @@
 
-;; TODO yasnippet
-
-;; TODO (auto-insert-mode 1)
-;; (define-auto-insert 'sh-mode 'ym-auto-insert-action)  ;; probably a good idea to use yasnippet
-
 
 ;; -------------------------------------------------------------------
 
